919f7ced144053f34411b193d02c153a0b11905b
viniciusBTT <vini.barrost@gmail.com>
2023-11-24 15:46:31 -0300

    UPDATE: Adicionando as configurações do CORS

--- src/main/java/com/br/sunioweb/editais/controller/AuthController.java
+++ src/main/java/com/br/sunioweb/editais/controller/AuthController.java
@@ -18,6 +18,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/auth")
+// Permitir solicitações do seu frontend (localhost)
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
